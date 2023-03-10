dd4150d8afa67193c8f1c6eebba13769a2b65d0e
Paige Nelmes <designer@paigenelmes.com>
2023-03-09 21:25:06 -0800

    Add exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "David", last_name: "Hall", hourly_rate: 70)
+@store1.employees.create(first_name: "Mai", last_name: "Tiger", hourly_rate: 50)
+@store2.employees.create(first_name: "Polly", last_name: "Walnuts", hourly_rate: 65)
+@store2.employees.create(first_name: "Barley", last_name: "Nelmes", hourly_rate: 45)
+@store2.employees.create(first_name: "Keiko", last_name: "Shiba", hourly_rate: 55)
+
